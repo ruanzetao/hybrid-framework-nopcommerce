907cfddc52522fceffca609a3aad70d99d2c5914
ruanzetao <ruanzetao@gmail.com>
2022-08-07 12:01:28 +0700

    Framework: DRY pattern

--- testcases/com/nopcommerce/user/User_00_Template.java
+++ testcases/com/nopcommerce/user/User_00_Template.java
@@ -4,15 +4,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class User_01_Register_Login {
-
+public class User_00_Template {
+	
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
-
 	@BeforeClass
 	public void beforeClass() {
 
@@ -23,32 +23,11 @@ public class User_01_Register_Login {
 		driver.manage().window().maximize();
 
 	}
-
-	@Test
-	public void TC_01() {
-	}
-
-	@Test
-	public void TC_02() {
-	}
-
-	@Test
-	public void TC_03() {
-	}
-
-	@Test
-	public void TC_04() {
-	}
-
-	@Test
-	public void TC_05() {
-	}
-
-	@Test
-	public void TC_06() {
-	}
-
-	@AfterClass
+  @Test
+  public void TC_01() {
+  }
+  
+  @AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
