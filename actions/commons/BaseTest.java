dcc0f7ef108489e0943c545b27c98b583a427408
ruanzetao <ruanzetao@gmail.com>
2022-08-26 22:57:16 -0700

    Framework: Multiple browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,31 @@
 package commons;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseTest {
+	protected WebDriver driver;
+	private String projectPath = System.getProperty("user.dir");
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driver = new FirefoxDriver();
+		} else {
+			throw new RuntimeException("Browser invalid");
+		}
+
+		// Init basePage
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().window().maximize();
+
+		return driver;
+	}
 
 }
